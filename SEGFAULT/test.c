c85e62f23e7e54fa84a523679c97c84f7171983a
lsflp <lipeluiis@hotmail.com>
2016-11-27 21:47:51 -0200

    Um segfault maroto

--- SEGFAULT/test.c
+++ SEGFAULT/test.c
@@ -5,7 +5,8 @@ int main () {
 	
 	ppmimg M;
 	int i, j, k;
-	M = readImage ("img.ppm");
+	M = readImage ("Bali.ppm");
+	printf("Li o arquivo.\n");
 	writeImage (M, "new.ppm");
 	freeImage (M);
 	return 0;
